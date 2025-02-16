afebdcc106e9143445d97b53e6233004aeb1b5c3
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-02-16 15:09:15 +0100

    working

--- CPP00/megaphone.cpp
+++ CPP00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:45:08 by mbany             #+#    #+#             */
-/*   Updated: 2025/02/15 17:15:59 by mbany            ###   ########.fr       */
+/*   Updated: 2025/02/16 14:59:55 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,19 @@ $>
 */
 #include <iostream>
 
-int s_len(char *s)
+void ft_toupper(char *s)
 {
     int i = 0;
     while (s[i])
-        i++;
-    return (i);
-}
-
-int rev_print(char *s)
-{
-    int len = s_len(s) - 1; // Ostatni znak jest na pozycji s_len - 1
-
-    while (len >= 0) // Pętla do 0, aby nie wyjść poza zakres
     {
-        std::cout << s[len]; // Wypisz pojedynczy znak
-        len--;
+        if (s[i] >= 'a' && s[i] <= 'z') 
+            std::cout << (char)(s[i] - 32);
+        else
+            std::cout << s[i]; 
+        i++;
     }
-    std::cout << std::endl; // Nowa linia
-    return (0);
 }
+
 
 int main(int ac, char **av)
 {
@@ -52,10 +45,10 @@ int main(int ac, char **av)
         int i = 1;
         while (i < ac)
         {
-            char *s = av[i]; // Pobierz kolejne argumenty
-            rev_print(s);    // Odwróć i wypisz
+            ft_toupper(av[i]);
             i++;
         }
+        std::cout << std::endl;
     }
     return 0;
 }
