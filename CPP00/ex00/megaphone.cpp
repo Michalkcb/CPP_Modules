16b6a2e3b31fac1fdf5b78efc467abefd193d2d3
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-02-18 20:11:16 +0100

    done megaphone

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,1 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 15:45:08 by mbany             #+#    #+#             */
+/*   Updated: 2025/02/16 14:59:55 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+/*
+Just to make sure that everybody is awake, write a program that behaves as follows:
+$>./megaphone "shhhhh... I think the students are asleep..."
+SHHHHH... I THINK THE STUDENTS ARE ASLEEP...
+$>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
+$>./megaphone
+* LOUD AND UNBEARABLE FEEDBACK NOISE *
+$>
+*/
+#include <iostream>
+
+void ft_toupper(char *s)
+{
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] >= 'a' && s[i] <= 'z') 
+            std::cout << (char)(s[i] - 32);
+        else
+            std::cout << s[i]; 
+        i++;
+    }
+}
+
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        int i = 1;
+        while (i < ac)
+        {
+            ft_toupper(av[i]);
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
