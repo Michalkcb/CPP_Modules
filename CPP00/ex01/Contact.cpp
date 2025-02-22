cd64dbd5db76ff95f718835589790d91c48c95e3
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-02-22 17:00:27 +0100

    update

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -6,20 +6,11 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 13:56:57 by mbany             #+#    #+#             */
-/*   Updated: 2025/02/22 13:59:54 by mbany            ###   ########.fr       */
+/*   Updated: 2025/02/22 14:28:23 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-class Contact
-{
-	public:
-		setFirstName(std::string s);
-		setLastName(std::string s);
-		setNickName(std::string s);
-		setNumber(std::string s);
-		setSecret(std::string s);
-	
+# include "Contact.hpp"
 
-	private:
-
-}
+std::string Contact::get_fname() const
+{ return m_fname;}
