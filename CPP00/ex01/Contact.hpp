55fb97242db76cd957d080f3411de4b44d447ab3
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-02-22 17:00:40 +0100

    update

---
+++ CPP00/ex01/Contact.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 13:56:57 by mbany             #+#    #+#             */
+/*   Updated: 2025/02/22 14:02:15 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Contact
+{
+	public:
+		setFirstName(std::string s);
+		setLastName(std::string s);
+		setNickName(std::string s);
+		setNumber(std::string s);
+		setSecret(std::string s);
+	
+
+	private:
+
+}
