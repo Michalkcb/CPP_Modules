b279f944aee9c3f3c7d6bcc254ec819df7f11d77
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-02-23 17:04:49 +0100

    update

--- CPP00/ex01/phonebook.hpp
+++ CPP00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 18:44:12 by mbany             #+#    #+#             */
-/*   Updated: 2025/02/22 14:09:25 by mbany            ###   ########.fr       */
+/*   Updated: 2025/02/23 17:01:49 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,22 @@
 void init_menu();
 int is_number(std::string str);
 
+class Contact
+{
+	public:
+		void setFirstName(std::string s);
+		void setLastName(std::string s);
+		void setNickName(std::string s);
+		void setNumber(std::string s);
+		void setSecret(std::string s);
+		void setid(int nb);
+	
+
+	private:
+	int m_id;
+	std::string mFirstName;
+
+};
 
 class PhoneBook
 {
@@ -29,6 +45,6 @@ class PhoneBook
 		int showContact(int index);
 
 	private:
-		// Contact user[8];
+		Contact user[8];
 		int nb_Contact;
 };
