0ab6813ef5ae0c63d990270b0a3954cda57b05b9
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-04-22 17:46:16 +0200

    change while -> for

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:45:08 by mbany             #+#    #+#             */
-/*   Updated: 2025/02/16 14:59:55 by mbany            ###   ########.fr       */
+/*   Updated: 2025/04/22 17:39:19 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,13 @@ $>./megaphone
 $>
 */
 #include <iostream>
+#include <cctype>
 
 void ft_toupper(char *s)
 {
-    int i = 0;
-    while (s[i])
-    {
-        if (s[i] >= 'a' && s[i] <= 'z') 
-            std::cout << (char)(s[i] - 32);
-        else
-            std::cout << s[i]; 
-        i++;
-    }
+    for (int i = 0; s[i]; i++)
+        std::cout << (char)toupper(s[i]);
 }
-
 
 int main(int ac, char **av)
 {
@@ -42,12 +35,8 @@ int main(int ac, char **av)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
-        int i = 1;
-        while (i < ac)
-        {
+        for (int i = 1; i < ac; i++)
             ft_toupper(av[i]);
-            i++;
-        }
         std::cout << std::endl;
     }
     return 0;
