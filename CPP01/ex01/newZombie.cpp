3747a6bd4ebf250d8344688ff29a3041d085552e
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-04-29 20:07:22 +0200

    update

--- CPP01/ex01/newZombie.cpp
+++ CPP01/ex01/newZombie.cpp
@@ -6,13 +6,21 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 16:08:01 by mbany             #+#    #+#             */
-/*   Updated: 2025/04/27 16:30:54 by mbany            ###   ########.fr       */
+/*   Updated: 2025/04/29 19:54:32 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+//utworzony obiekt istnieje poza funkcją
+//nie jest automatycznie niszczony więc trzeba go dniszczyć po wywołaniu
+// obiekt jest tworzony na stercie (heap)
 
-Zombie* newZombie(std::string name)
+Zombie* zombieHorde( int N, std::string name )
 {
-	return new Zombie(name);
+	Zombie* horde = new Zombie[N];
+	for (int i = 0; i < N; i++)
+	{
+		horde[i].setName(name);
+	}
+	return horde;
 }
