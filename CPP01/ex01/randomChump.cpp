8a45f1f562875c5d128ae90f52a48808c6d2a8da
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-04-29 20:07:09 +0200

    update

--- CPP01/ex01/randomChump.cpp
+++ CPP01/ex01/randomChump.cpp
@@ -6,11 +6,14 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 16:08:04 by mbany             #+#    #+#             */
-/*   Updated: 2025/04/27 16:32:27 by mbany            ###   ########.fr       */
+/*   Updated: 2025/04/29 19:12:37 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+// utworzony obiekt istnieje tylko w zakresie funkcji
+// obiekt jest automatycznie usuwany po zakończeniu funkcji
+// obiekt jest tworzony na stosie (stack)
 
 void randomChump(std::string name)
 {
