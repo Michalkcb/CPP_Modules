9baba19ba9d61b2afa3e3aec1fa1bb193325b46a
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-04-29 20:06:57 +0200

    update

--- CPP01/ex01/Zombie.cpp
+++ CPP01/ex01/Zombie.cpp
@@ -6,21 +6,30 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 16:08:07 by mbany             #+#    #+#             */
-/*   Updated: 2025/04/27 16:31:05 by mbany            ###   ########.fr       */
+/*   Updated: 2025/04/29 20:01:46 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+Zombie::Zombie() : name("")
+{
+}
+
 Zombie::Zombie(std::string name) : name(name)
 {
 }
+
 Zombie::~Zombie()
 {
 	std::cout << "Zombie " << name << " destroyed." << std::endl;
 }
+void Zombie::setName(std::string name)
+{
+	this->name = name;
+}
 
 void Zombie::announce(void) const
 {
-	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << name << ": MooooaaaaR BraiiiiiiinnnzzzZ..." << std::endl;
 }
