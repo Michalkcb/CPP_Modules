30e85c5119af4766b7e54c298be94726c6cf595d
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-05-10 15:37:23 +0200

    update final

--- CPP01/ex03/HumanA.hpp
+++ CPP01/ex03/HumanA.hpp
@@ -6,7 +6,26 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 00:05:30 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/09 00:05:32 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/10 14:01:01 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include <iostream>
+#include <string>
+#include "Weapon.hpp"
+
+class HumanA
+{
+private:
+	std::string name;
+	Weapon &weapon;
+	
+public:
+	HumanA(const std::string &name, Weapon &weapon);
+	void attack() const;
+};
+
+#endif
