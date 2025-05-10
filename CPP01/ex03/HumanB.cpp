4b73e7b8cbe0007312586a50d0f9b05d7d95fb58
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-05-10 15:37:59 +0200

    update final

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -6,7 +6,23 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 00:06:10 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/09 00:06:11 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/10 15:27:12 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanB.hpp"
+
+HumanB::HumanB(const std::string &name) : name(name), weapon(NULL)
+{
+}
+void HumanB::setWeapon(Weapon &newWeapon)
+{
+	this->weapon = &newWeapon;
+}
+void HumanB::attack() const
+{
+	if (this->weapon != NULL)
+		std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+	else
+		std::cout << name << " has no weapon to attack with!" << std::endl;
+}
