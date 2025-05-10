d45ec7db16d07b5dbf070b5af6b02c114ba04564
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-05-10 15:41:31 +0200

    update final

--- CPP01/ex03/Weapon.hpp
+++ CPP01/ex03/Weapon.hpp
@@ -6,7 +6,22 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 00:04:53 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/09 00:04:54 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/10 13:50:45 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+#include <iostream>
+
+class Weapon
+{
+private:
+	std::string type;
+
+public:
+	Weapon(const std::string &type);
+	const std::string &getType() const;
+	void setType(const std::string& newType);
+};
+#endif
