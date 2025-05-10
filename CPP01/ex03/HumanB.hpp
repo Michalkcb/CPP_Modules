0f818f40172bf80b3044bb85e8ff1e7788ecd537
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-05-10 15:40:32 +0200

    update final

--- CPP01/ex03/HumanB.hpp
+++ CPP01/ex03/HumanB.hpp
@@ -6,7 +6,27 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 00:06:00 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/09 00:06:01 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/10 15:26:59 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+#include <iostream>
+#include <string>
+#include "Weapon.hpp"
+
+class HumanB
+{
+private:
+	std::string name;
+	Weapon *weapon;
+	
+public:
+	HumanB(const std::string &name);
+	void setWeapon(Weapon &weapon);
+	void attack() const;
+};
+
+#endif
