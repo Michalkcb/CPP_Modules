6a017da1028c2b1be596159846eb6514f1809a8e
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-05-10 15:33:30 +0200

    update final

---
+++ CPP01/ex03/HumanA.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 13:57:04 by mbany             #+#    #+#             */
+/*   Updated: 2025/05/10 13:57:54 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+
+HumanA::HumanA(const std::string &name, Weapon &weapon) : name(name), weapon(weapon)
+{
+}
+void HumanA::attack() const
+{
+	std::cout << name << " attacks with their " << weapon.getType() << std::endl;
+}
