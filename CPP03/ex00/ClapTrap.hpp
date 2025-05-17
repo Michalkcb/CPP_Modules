19c27adffd3d72c83d81801bb6f9fdd3be4f1fa1
michalkcb <michalkcb@gmail.com>
2025-05-17 15:03:16 +0200

    init clap class

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,37 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 14:53:06 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/17 14:53:08 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/17 15:02:31 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+private:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+public:
+	ClapTrap(/* args */);
+	~ClapTrap();
+	
+	void attack(const std::string& target);	
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+ClapTrap::ClapTrap(/* args */)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
+
+#endif
