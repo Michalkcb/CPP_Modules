f47267fe8b9b61eaeb8b23749466219c9e0fbac4
michalkcb <michalkcb@gmail.com>
2025-05-25 15:47:13 +0200

    init brain

---
+++ CPP04/ex00/Brain.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 14:53:06 by mbany             #+#    #+#             */
+/*   Updated: 2025/05/25 15:46:21 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef Cat_HPP
+#define Cat_HPP
+#include "Animal.hpp"
+#include <iostream>
+#include <string>
+
+class Brain
+{
+public:
+	Brain();
+	~Brain();
+	Brain(const Brain &other);
+};
+
+#endif
