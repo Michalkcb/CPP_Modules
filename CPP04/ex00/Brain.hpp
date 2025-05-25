afb93c9928284b3cb35c89baf12e1cee649ea020
michalkcb <michalkcb@gmail.com>
2025-05-25 15:55:24 +0200

    add private attribute

--- CPP04/ex00/Brain.hpp
+++ CPP04/ex00/Brain.hpp
@@ -6,22 +6,25 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 14:53:06 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/25 15:46:21 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/25 15:54:14 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Cat_HPP
-#define Cat_HPP
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
 #include "Animal.hpp"
 #include <iostream>
 #include <string>
 
 class Brain
 {
-public:
-	Brain();
-	~Brain();
-	Brain(const Brain &other);
+	private:
+		std::string ideas[100];
+	public:
+		Brain();
+		~Brain();
+		Brain(const Brain &other);
+		Brain &operator=(const Brain &other);
 };
 
 #endif
