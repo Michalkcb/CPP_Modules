0ed411e386ddcc7ad31df84422ee9ce482443861
Michael <michalkcb@gmail.com>
2025-05-29 19:59:02 +0200

    make wrong cat

--- CPP04/ex00/WrongCat.hpp
+++ CPP04/ex00/WrongCat.hpp
@@ -6,7 +6,19 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 19:28:18 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/29 19:28:20 by mbany            ###   ########.fr       */
+/*   Updated: 2025/05/29 19:58:43 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal {
+public:
+    WrongCat();
+    ~WrongCat();
+    void makeSound() const;
+};
+
+#endif
