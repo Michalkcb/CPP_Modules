ec1ae9953356cc4a5fa1227b38b2e10ec913a4d2
michalkcb <michalkcb@gmail.com>
2025-06-01 15:10:06 +0200

    init ex03

--- CPP04/ex03/AMateria.cpp
+++ CPP04/ex03/AMateria.cpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/17 14:53:06 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/25 14:50:01 by mbany            ###   ########.fr       */
+/*   Created: 2025/06/01 15:09:48 by mbany             #+#    #+#             */
+/*   Updated: 2025/06/01 15:09:49 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DOG_HPP
-#define DOG_HPP
-#include "Animal.hpp"
-#include <iostream>
-#include <string>
-
-class Dog : public Animal
-{
-public:
-	Dog();
-	~Dog();
-	void makeSound() const;
-};
-
-#endif
--- CPP04/ex03/AMateria.hpp
+++ CPP04/ex03/AMateria.hpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/17 14:53:06 by mbany             #+#    #+#             */
-/*   Updated: 2025/05/25 14:59:47 by mbany            ###   ########.fr       */
+/*   Created: 2025/06/01 15:09:50 by mbany             #+#    #+#             */
+/*   Updated: 2025/06/01 15:09:51 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef Cat_HPP
-#define Cat_HPP
-#include "Animal.hpp"
-#include <iostream>
-#include <string>
-
-class Cat : public Animal
-{
-public:
-	Cat();
-	~Cat();
-	void makeSound() const;
-};
-
-#endif
