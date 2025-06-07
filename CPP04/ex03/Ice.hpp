59772bd3c8da373985d64977648c240328aa1caf
Michael <michalkcb@gmail.com>
2025-06-07 22:22:03 +0200

    done ice hpp

--- CPP04/ex03/Ice.hpp
+++ CPP04/ex03/Ice.hpp
@@ -6,7 +6,26 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 21:23:15 by mbany             #+#    #+#             */
-/*   Updated: 2025/06/07 21:23:16 by mbany            ###   ########.fr       */
+/*   Updated: 2025/06/07 22:21:37 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ICE_HPP
+#define ICE_HPP
+
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+class Ice : public AMateria {
+    public:
+        Ice();
+        virtual ~Ice();
+        Ice(Ice const & other);
+        Ice & operator=(Ice const & other);
+
+        virtual AMateria* clone() const;
+        virtual void use(ICharacter& target);
+
+};
+
+#endif
