76dbb58e07a3ca38492949f411f4ff93c60b378f
Michael <michalkcb@gmail.com>
2025-06-07 22:27:54 +0200

    done ice cpp

--- CPP04/ex03/Ice.cpp
+++ CPP04/ex03/Ice.cpp
@@ -6,7 +6,33 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 21:23:43 by mbany             #+#    #+#             */
-/*   Updated: 2025/06/07 21:23:45 by mbany            ###   ########.fr       */
+/*   Updated: 2025/06/07 22:27:33 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Ice.hpp"
+#include <iostream>
+
+Ice::Ice() : AMateria("ice") {};
+
+Ice::~Ice() {};
+Ice::Ice(Ice const & other) : AMateria(other) {};
+
+Ice & Ice::operator=(Ice const & other) {
+    if (this != &other) {
+        this->type = other.type;
+    }
+    return *this;
+}
+
+AMateria* Ice::clone() const {
+    return new Ice(*this);
+}
+
+AMateria* Ice::clone() const {
+    return new Ice(*this);
+}
+
+void Ice::use(ICharacter& target) {
+    std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+}
