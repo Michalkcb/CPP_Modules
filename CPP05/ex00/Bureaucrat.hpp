f08861789d99dc07efce89ce6e8472c01e92486d
michalkcb <michalkcb@gmail.com>
2025-06-30 17:51:04 +0200

    first version of Bureaucrat class

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,26 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 17:42:43 by mbany             #+#    #+#             */
-/*   Updated: 2025/06/30 17:42:44 by mbany            ###   ########.fr       */
+/*   Updated: 2025/06/30 17:50:43 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+class Bureaucrat 
+{
+protected:
+	const std::string name;
+	int grade;
+
+public:
+	Bureaucrat();
+	~Bureaucrat();
+	Bureaucrat(const Bureaucrat &other);
+	Bureaucrat &operator=(const Bureaucrat &other);
+
+	std::string getName() const;
+	int getGrade() const;
+	
+};
