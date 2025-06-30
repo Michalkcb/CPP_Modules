fe9887cf6133e86eeeabb2473a127c399a031d84
michalkcb <michalkcb@gmail.com>
2025-06-30 20:11:56 +0200

    change main

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,49 +6,43 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 00:01:36 by mbany             #+#    #+#             */
-/*   Updated: 2025/06/30 19:13:10 by mbany            ###   ########.fr       */
+/*   Updated: 2025/06/30 19:47:10 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 
-Exercise 00: Mommy, when I grow
-up, I want to be a bureaucrat!
-Exercise : 00
-Mommy, when I grow up, I want to be a bureaucrat!
-Turn-in directory : ex00/
-Files to turn in : Makefile, main.cpp, Bureaucrat.{h, hpp}, Bureaucrat.cpp
+Exercise 01: Form up, maggots!
+Exercise : 01
+Form up, maggots!
+Turn-in directory : ex01/
+Files to turn in : Files from previous exercise + Form.{h, hpp}, Form.cpp
 Forbidden functions : None
-Please note that exception classes don’t have to be designed in
-Orthodox Canonical Form. But every other class has to.
-Let’s design an artificial nightmare of offices, corridors, forms, and waiting queues.
-Sounds fun? No? Too bad.
-First, start by the smallest cog in this vast bureaucratic machine: the Bureaucrat.
-A Bureaucrat must have:
+Now that you have bureaucrats, let’s give them something to do. What better activity
+could there be than the one of filling out a stack of forms?
+Then, let’s make a Form class. It has:
 • A constant name.
-• And a grade that ranges from 1 (highest possible grade) to 150 (lowest possible
-grade).
-Any attempt to instantiate a Bureaucrat using an invalid grade must throw an exception:
-either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException.
-6
+• A boolean indicating whether it is signed (at construction, it’s not).
+• A constant grade required to sign it.
+• And a constant grade required to execute it.
+All these attributes are private, not protected.
+The grades of the Form follow the same rules that apply to the Bureaucrat. Thus,
+the following exceptions will be thrown if a form grade is out of bounds:
+Form::GradeTooHighException and Form::GradeTooLowException.
+Same as before, write getters for all attributes and an overload of the insertion («)
+operator that prints all the form’s informations.
+8
 C++ - Module 05 Repetition and Exceptions
-You will provide getters for both these attributes: getName() and getGrade(). Implement also two member functions to increment or decrement the bureaucrat grade. If
-the grade is out of range, both of them will throw the same exceptions as the constructor.
-Remember. Since grade 1 is the highest one and 150 the lowest,
-incrementing a grade 3 should give a grade 2 to the bureaucrat.
-The thrown exceptions must be catchable using try and catch blocks:
-try
-{
-do some stuff with bureaucrats
-}
-catch (std::exception & e)
-{
-handle exception
-}
-You will implement an overload of the insertion («) operator to print something like
-(without the angle brackets):
-<name>, bureaucrat grade <grade>.
-As usual, turn in some tests to prove everything works as expected.
+Add also a beSigned() member function to the Form that takes a Bureaucrat as
+parameter. It changes the form status to signed if the bureaucrat’s grade is high enough
+(higher or egal to the required one). Remember, grade 1 is higher than grade 2.
+If the grade is too low, throw a Form::GradeTooLowException.
+Then, modify the signForm() member function in the Bureaucrat class. This function must call Form::beSigned() to attempt to sign the form. If the form is signed
+successfully, it will print something like:
+<bureaucrat> signed <form>
+Otherwise, it will print something like:
+<bureaucrat> couldn’t sign <form> because <reason>.
+Implement and turn in some tests to ensure everything works as expected.
 
 */
 
