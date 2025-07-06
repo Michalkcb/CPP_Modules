662e46f1b7d3afe2ba42b577f4187d41c4a9b4ba
michalkcb <michalkcb@gmail.com>
2025-07-06 16:02:50 +0200

    init main

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,7 +6,27 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:02:04 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/06 16:02:05 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/06 16:02:44 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Exercise 01: Serialization
+Exercise : 01
+Serialization
+Turn-in directory : ex01/
+Files to turn in : Makefile, *.cpp, *.{h, hpp}
+Forbidden functions : None
+Implement a class Serializer, who will not be initializable by the user by any way,
+with the following static methods:
+uintptr_t serialize(Data* ptr);
+It takes a pointer and converts it to the unsigned integer type uintptr_t.
+Data* deserialize(uintptr_t raw);
+It takes an unsigned integer parameter and converts it to a pointer to Data.
+Write a program to test that your class works as expected.
+You must create a non-empty (it means it has data members) Data structure.
+Use serialize() on the address of the Data object and pass its return value to
+deserialize(). Then, ensure the return value of deserialize() compares equal to the
+original pointer.
+Do not forget to turn in the files of your Data structure.
+*/
