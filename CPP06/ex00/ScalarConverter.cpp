d90911a8376f03438c7ba0e7e0a02bf693f35b08
michalkcb <michalkcb@gmail.com>
2025-07-08 19:30:55 +0200

    add function choosing converter

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,9 +6,33 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 18:55:51 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/08 18:56:18 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/08 19:29:47 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+#include <cctype>
+
+ScalarConverter :: ScalarConverter() {};
+ScalarConverter :: ScalarConverter(const ScalarConverter&) {};
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter&) { return *this;}// Prevent assignment
+ScalarConverter :: ~ScalarConverter() {};
+
+void ScalarConverter::convert(const std::string &literal) {
+	if (isChar(literal)) {
+		convertChar(literal);
+	} else if (isInt(literal)) {
+		convertInt(literal);
+	} else if (isFloat(literal)) {
+		convertFloat(literal);
+	} else if (isDouble(literal)) {
+		convertDouble(literal);
+	} else {
+		std::cerr << "Invalid literal: " << literal << std::endl;
+	}
+}
+
+bool ScalarConverter::isChar(const std::string &literal) {
+	return literal.length() == 1 && isprint(literal[0]) && !isdigit(literal[0]);
+}
 
