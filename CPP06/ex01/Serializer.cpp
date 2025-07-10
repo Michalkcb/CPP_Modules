db909dbaec4c34a8ccd9f7a6e33076958148f681
michalkcb <michalkcb@gmail.com>
2025-07-10 19:19:18 +0200

    Serializer.cpp done

--- CPP06/ex01/Serializer.cpp
+++ CPP06/ex01/Serializer.cpp
@@ -6,9 +6,16 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 19:11:54 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/10 19:12:17 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/10 19:16:14 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
+uintptr_t Serializer::serialize(Data* ptr) {
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* Serializer::deserialize(uintptr_t raw) {
+	return reinterpret_cast<Data*>(raw);
+}
