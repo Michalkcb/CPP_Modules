44d9cafae91741b1957e8d8eb600d2e96c7e9d55
michalkcb <michalkcb@gmail.com>
2025-07-10 19:11:15 +0200

    struct data done

--- CPP06/ex01/Data.hpp
+++ CPP06/ex01/Data.hpp
@@ -6,20 +6,14 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 18:57:53 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/10 18:58:46 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/10 19:10:48 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <string>
 
-class Data {
-public:
-	Data(const std::string &name, int value);
-	Data(const Data &other);
-	Data &operator=(const Data &other);
-	~Data();
-
-	const std::string &getName() const;
-	int getValue() const;
+struct Data {
+	int number;
+	std::string text;
 };
