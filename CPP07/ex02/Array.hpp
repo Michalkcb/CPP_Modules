09d5fa5e86230b2bf190cbb40f14cf3889cc55fd
michalkcb <michalkcb@gmail.com>
2025-07-13 15:29:38 +0200

    add Array methods

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,13 +6,24 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 15:08:34 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/13 15:09:39 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/13 15:27:22 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #pragma once
 
+template <typename T>
 class Array {
-	
-};
+	private:
+		T* _array;
+		unsigned int _size;
+
+	public:
+		Array();
+		Array(const Array<T>& other);
+		Array &operator=(const Array<T>& other);
+		~Array();
+		
+		Array(unsigned int n);
+	};
