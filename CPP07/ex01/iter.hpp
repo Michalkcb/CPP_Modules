e7e5678da09fd080c6bdc74da23149bd70cb6185
michalkcb <michalkcb@gmail.com>
2025-07-13 13:11:31 +0200

    change iter.hpp

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,15 +6,16 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 15:27:11 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/12 15:30:58 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/13 13:10:29 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <cstddef> // for size_t
-template <typename T>
-void iter(T* array, size_t length, void (*func)(T&)) {
-	for (size_t i = 0; i < length; ++i) {
-		func(array[i]);
-	}
+
+template <typename T, typename F>
+void iter(T* array, size_t length, F func) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
 }
