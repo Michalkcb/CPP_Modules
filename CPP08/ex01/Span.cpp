ce1563d02159d65ca02f899ff35051f24af0f2b1
michalkcb <michalkcb@gmail.com>
2025-07-19 18:47:51 +0200

    starting to work

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,11 +6,12 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 15:35:43 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/19 17:13:50 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/19 18:47:14 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <algorithm>
 
 Span::Span() : _maxSize(0) {}
 Span::Span(const Span &other) : _maxSize(other._maxSize), _numbers(other._numbers) {}
@@ -24,12 +25,26 @@ Span &Span::operator=(const Span &other) {
 Span::~Span() {}
 Span::Span(unsigned int n) : _maxSize(n) {}
 
-void addNumber(int number) {
-	
+void Span::addNumber(int number) {
+	if (_numbers.size() >= _maxSize) {
+		throw std::length_error("Span is full, cannot add more numbers.");
+	}
+	_numbers.push_back(number);
 }
-int shortestSpan() {
-	
+int Span::shortestSpan() const {
+	if (_numbers.size() < 2)
+		throw std::logic_error("Not enough numbers");
+	std::vector<int> tmp = _numbers;
+	std::sort(tmp.begin(), tmp.end());
+	int minSpan = tmp[1] - tmp[0];
+	for (size_t i = 1; i < tmp.size() - 1; ++i)
+		minSpan = std::min(minSpan, tmp[i + 1] - tmp[i]);
+	return minSpan;
 }
-int longestSpan() {
-	
+int Span::longestSpan() const {
+	if (_numbers.size() < 2)
+		throw std::logic_error("Not enough numbers");
+	int minVal = *std::min_element(_numbers.begin(), _numbers.end());
+	int maxVal = *std::max_element(_numbers.begin(), _numbers.end());
+	return maxVal - minVal;
 }
