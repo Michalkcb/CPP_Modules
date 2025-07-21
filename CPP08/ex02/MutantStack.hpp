fd49eaaeb122fc1cbfcea1c2c6ccd3d7be3e6bd0
michalkcb <michalkcb@gmail.com>
2025-07-21 18:22:47 +0200

    init mutantstack

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,18 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 18:18:53 by mbany             #+#    #+#             */
-/*   Updated: 2025/07/21 18:18:54 by mbany            ###   ########.fr       */
+/*   Updated: 2025/07/21 18:22:36 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <stack>
+
+class MutantStack
+{
+private:
+	/* data */
+public:
+	MutantStack(/* args */);
+	~MutantStack();
+};
