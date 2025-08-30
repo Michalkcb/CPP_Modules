c0e9269251133ee9215622035e9a4044648a39aa
michalkcb <michalkcb@gmail.com>
2025-08-30 16:25:49 +0200

    init processInput, run, main

--- CPP09/ex01/PmergeMe.cpp
+++ CPP09/ex01/PmergeMe.cpp
@@ -5,8 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/30 10:47:43 by mbany             #+#    #+#             */
-/*   Updated: 2025/08/30 10:47:45 by mbany            ###   ########.fr       */
+/*   Created: 2025/08/30 10:57:46 by mbany             #+#    #+#             */
+/*   Updated: 2025/08/30 16:14:43 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& other) : _vectorData(other._vectorData), _dequeData(other._dequeData) {
+	
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
+	if (this != &other) {
+		_vectorData = other._vectorData;
+		_dequeData = other._dequeData;
+	}
+	return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+void PmergeMe::processInput(int argc, char ** argv) {
+	(void)argc;
+	(void)argv;
+	std::cout << "Processing input..." << std::endl;
+}
+
+void PmergeMe::run() {
+	std::cout << "Running PmergeMe..." << std::endl;
+}
